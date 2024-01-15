d570b8f95d83cb7c0465c12c67739d2b98afe975
Majedah <122641068+majowl77@users.noreply.github.com>
2024-01-16 01:59:19 +0300

    fixing some style and logic

--- src/components/postDetails/PostDetails.tsx
+++ src/components/postDetails/PostDetails.tsx
@@ -14,6 +14,8 @@ export default function PostDetails() {
   const postDetails = useSelector(
     (state: RootState) => state.postsReducer.singlePost
   );
+  const postExists = posts.postsList.some((post) => post.id === Number(Id));
+
   useEffect(() => {
     const handleGetPosts = async () => {
       dispatch(getSingleUserPostsThunk(Number(Id)));
@@ -23,51 +25,61 @@ export default function PostDetails() {
 
   return (
     <div>
-      <div className="absolute top-16 left-60 px-2 py-3 text-2xl leading-tight text-left ">
-        <Link to={`/posts/user-posts/${postDetails?.userId}`}>
-          <BackButton />
-        </Link>
-      </div>
-      <div className=" mx-64 ">
-        <h2 className=" px-2 py-3 text-2xl leading-tight text-left">
-          Post Details
-        </h2>
-        <div className="rounded-xl border-2 border-gray-100 bg-white text-start">
-          {posts.isLoading ? (
-            <div className=" h-80 w-96 bg-white flex items-center justify-center overflow-auto">
-              <div>
-                <Spinner />
-              </div>
-            </div>
-          ) : (
-            <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8 w-180">
-              <div>
-                <h1 className="font-medium sm:text-lg  text-indigo-400">
-                  {postDetails?.title}
-                </h1>
-
-                <p className="line-clamp-2 text-m text-gray-700 ">
-                  {postDetails?.body}
-                </p>
-
-                <div className="mt-2 sm:flex sm:items-center sm:gap-2">
-                  <div className="flex items-center gap-1 text-gray-500">
-                    <p className="text-s"> User Id {postDetails?.userId}</p>
+      {postExists ? (
+        <div>
+          <div className="absolute top-16 left-60 px-2 py-3 text-2xl leading-tight text-left ">
+            <Link to={`/posts/user-posts/${postDetails?.userId}`}>
+              <BackButton />
+            </Link>
+          </div>
+          <div className=" mx-64 ">
+            <h2 className=" px-2 py-3 text-2xl leading-tight text-left">
+              Post Details
+            </h2>
+            <div className="rounded-xl border-2 border-gray-100 bg-white text-start">
+              {posts.isLoading ? (
+                <div className=" h-80 w-96 bg-white flex items-center justify-center overflow-auto">
+                  <div>
+                    <Spinner />
                   </div>
-
-                  <span className="hidden sm:block" aria-hidden="true">
-                    &middot;
-                  </span>
-
-                  <p className="hidden sm:block sm:text-s sm:text-gray-500">
-                    Post Id {postDetails?.id}
-                  </p>
                 </div>
-              </div>
+              ) : (
+                <div className="flex items-start gap-4 p-4 sm:p-6 lg:p-8 w-180">
+                  <div>
+                    <h1 className="font-medium sm:text-lg  text-indigo-400">
+                      {postDetails?.title}
+                    </h1>
+
+                    <p className="line-clamp-2 text-m text-gray-700 ">
+                      {postDetails?.body}
+                    </p>
+
+                    <div className="mt-2 sm:flex sm:items-center sm:gap-2">
+                      <div className="flex items-center gap-1 text-gray-500">
+                        <p className="text-s"> User Id {postDetails?.userId}</p>
+                      </div>
+
+                      <span className="hidden sm:block" aria-hidden="true">
+                        &middot;
+                      </span>
+
+                      <p className="hidden sm:block sm:text-s sm:text-gray-500">
+                        Post Id {postDetails?.id}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className=" text-4xl ">
+          <h1 className="uppercase tracking-widest text-gray-400">
+            404 | Post Id Not Found
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
